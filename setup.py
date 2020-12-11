6a7c705d4e213f8013f835601b602c96124ef71d
Yusheng Cai <cys9741@seas.upenn.edu>
2020-12-11 14:40:28 -0500

    added setup file

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+import numpy
+
+extensions = [Extension("analysis_code.md",["analysis_code/md.pyx"],\
+            include_dirs=[numpy.get_include()], \
+            define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]), \
+            Extension("analysis_code.timeseries",["analysis_code/timeseries.pyx"],\
+            include_dirs=[numpy.get_include()], \
+            define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]),\
+        Extension("analysis_code.rdf",["analysis_code/rdf.pyx"],\
+            include_dirs=[numpy.get_include()], \
+            define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]), \
+        Extension("analysis_code.Liquid_crystal.Liquid_crystal",["analysis_code/Liquid_crystal/Liquid_crystal.pyx"],\
+            include_dirs=[numpy.get_include()], \
+            define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]), \
+        Extension("analysis_code.Liquid_crystal.INDUS_utils",["analysis_code/Liquid_crystal/INDUS_util.pyx"],\
+            include_dirs=[numpy.get_include()], \
+            define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")])]
+
+setup(name='analysis_code',ext_modules = cythonize(extensions,compiler_directives={'language_level' : "3"}))
