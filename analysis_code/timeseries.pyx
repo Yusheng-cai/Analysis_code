4b7b83726a1a3e9d7d1fb0e1b8e9b5066082c377
Yusheng Cai <cys9741@seas.upenn.edu>
2021-01-18 17:10:09 -0500

    adjusted the autocorrelation time tau in AC_tau() function in timeseries

--- analysis_code/timeseries.pyx
+++ analysis_code/timeseries.pyx
@@ -6,11 +6,11 @@ class Timeseries:
     def __init__(self,data,time):
         """
         data: the Time series data that user inputs (shape: (N,...)) where N is the number of time steps
-        time: total time length
+        time: total time length (numpy array)
         """
+        self.n = len(data)
         self.time = time
         self.data = data
-        self.n = len(data)
 
     def blockstats(self,size_block,num_draws):
         cdef np.ndarray block = np.zeros((num_draws,))
@@ -38,8 +38,7 @@ class Timeseries:
         """
         ignore the first (window) points and calculate the moving average
         """
-        cdef np.ndarray filter = np.ones((window,))*(1/window) 
-        cdef np.ndarray smoothed_points 
+        filter = np.ones((window,))*(1/window) 
         smoothed_points = np.convolve(self.data,filter,'valid')
         
         return Timeseries(smoothed_points,self.time[window-1:])
@@ -69,13 +68,15 @@ class Timeseries:
         N = len(normalized)
         lags = np.arange(0,N*delta_t,delta_t)
         
-        # First zero pad the data
+        # zero pad the data
         normalized = np.r_[normalized[N//2:],np.zeros_like(normalized),normalized[:N//2]]
 
         # First perform fourier transform on the data set
         ft = np.fft.fft(normalized)
+
         # Square the fourier transform output
         sq_ft = (ft.real)**2+(ft.imag)**2
+
         # Now perform inver fourier transform on the sq_ft
         AC = np.fft.ifft(sq_ft)
 
@@ -84,7 +85,7 @@ class Timeseries:
 
         return (lags,AC[:N])
 
-    def AC_tau(self):
+    def AC_tau(self,cutoff=None):
         """
         Function that calculates autocorrelation time of a time series according to the definition provided by 
         (Box and Jenkins 1976), where
@@ -94,10 +95,17 @@ class Timeseries:
                 autocorrelation time (float)
         """
         _,ac = self.autocorrelation()
-        sum_ = 0
-        for num in range(len(ac)):
-            sum_ += (1-num/self.n)*ac[num]
-
+        coeff_ = 1-np.arange(1,self.n+1)/self.n
+        
+        if cutoff is None:
+            sum_ = (coeff_*ac).sum()
+        else:
+            sum_ = 0
+            for i in range(len(ac)):
+                if ac[i] >= 0:
+                    sum_ += coeff_[i]*ac[i]
+                else:
+                    break
 
         return sum_
 
